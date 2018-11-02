61fabc72e723331e18b43a25a332b533b8a998e6
hashdogg <hashdoggie@gmail.com>
2018-11-02 03:41:48 +0000

    Started basic layout

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+
+    render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    <div className="home">
+
+          <body>
+              <header className="home-header">
+                  <h1>Currency Fair</h1>
+              </header>
+              <div id="page-wrap">
+                  <div className="home-main">
+                      <h2>
+                          content
+                      </h2>
+                      <p>
+                          This is content to be made
+                      </p>
+                  </div>
+              </div>
+          </body>
       </div>
     );
   }
