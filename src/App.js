45a9792be5061b672c7366099bbb8be91afbe579
hashdogg <hashdoggie@gmail.com>
2018-11-05 04:23:18 +0000

    fixed tabs, added transaction text and boxes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CFLogo from './CF_logo_RGB_NEG.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,19 +8,40 @@ class App extends Component {
     render() {
     return (
     <div className="home">
-
           <body>
               <header className="home-header">
-                  <h1>Currency Fair</h1>
+                  <img src={CFLogo}/>
               </header>
-              <div id="page-wrap">
-                  <div className="home-main">
-                      <h2>
-                          content
-                      </h2>
-                      <p>
-                          This is content to be made
-                      </p>
+              <div className="home-main">
+                  <div className="home-left">
+                      <div className="tab">
+                          <div className="tab-links link-1">
+                              <h5>Step 1</h5>
+                              <h4>Transaction Info</h4>
+                          </div>
+                          <div className="tab-links">
+                              <h5>Step 2</h5>
+                              <h4>Recipient Info</h4>
+                          </div>
+                          <div className="tab-links">
+                              <h5>Step 3</h5>
+                              <h4>Make Payment</h4>
+                          </div>
+                      </div>
+                      <div className="transaction">
+                              <h2>Let's set up your transaction!</h2>
+                              <h3>Specify the amount to be received.</h3>
+                          <div className="transaction-box box-1">
+                              <h2>You send</h2>
+                              <h1>€2,000.00</h1>
+                          </div>
+                          <div className="transaction-box">
+                              <h2>Receiver Gets</h2>
+                              <h1>€1,717.94</h1>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="home-panel">
                   </div>
               </div>
           </body>
