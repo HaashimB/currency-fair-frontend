6e0385325a3cec50df210b226ba679f1ce8b080e
hashdoge <barihaashim@gmail.com>
2018-11-09 02:07:39 +0000

    Updates

---
+++ precache-manifest.6aa6122d9c173120ebc47c434b8d6952.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c847cc95af69eeeef66",
+    "url": "/currency-fair-frontend/static/css/main.afe00dfe.chunk.css"
+  },
+  {
+    "revision": "0c847cc95af69eeeef66",
+    "url": "/currency-fair-frontend/static/js/main.0c847cc9.chunk.js"
+  },
+  {
+    "revision": "c994860c11e8bfb13543",
+    "url": "/currency-fair-frontend/static/js/1.c994860c.chunk.js"
+  },
+  {
+    "revision": "baa485e8e9303448a319",
+    "url": "/currency-fair-frontend/static/js/runtime~main.baa485e8.js"
+  },
+  {
+    "revision": "7b7e6ac84c851947f60593a4f0df0acf",
+    "url": "/currency-fair-frontend/static/media/CF_logo_RGB_NEG.7b7e6ac8.svg"
+  },
+  {
+    "revision": "03d6237b4c8d43871d47d21a6571b12d",
+    "url": "/currency-fair-frontend/static/media/lock.03d6237b.png"
+  },
+  {
+    "revision": "24b116460aae3b081e8c60916a1e15ab",
+    "url": "/currency-fair-frontend/static/media/uk.24b11646.png"
+  },
+  {
+    "revision": "9c5cd8254b3def118113e9d6137c753b",
+    "url": "/currency-fair-frontend/static/media/euro.9c5cd825.png"
+  },
+  {
+    "revision": "6338c90c5ce6cf1072687e7e5c0c8a0e",
+    "url": "/currency-fair-frontend/static/media/question.6338c90c.png"
+  },
+  {
+    "revision": "d843269392cdf3ba347c185cd82dec71",
+    "url": "/currency-fair-frontend/static/media/PostGrotesk-Light.d8432693.woff"
+  },
+  {
+    "revision": "73496d664aad07037a9c1c4dae8d2798",
+    "url": "/currency-fair-frontend/static/media/PostGrotesk-Book.73496d66.woff"
+  },
+  {
+    "revision": "b6a37764b3752b2ccfd35e51f93c5473",
+    "url": "/currency-fair-frontend/static/media/PostGrotesk-Medium.b6a37764.woff"
+  },
+  {
+    "revision": "febf504532ea00c046158055ce20f254",
+    "url": "/currency-fair-frontend/static/media/PostGrotesk-Bold.febf5045.woff"
+  },
+  {
+    "revision": "12185df249e3c354a060397cb8f096b8",
+    "url": "/currency-fair-frontend/index.html"
+  }
+];
